87e3096f759df33ab8f6c7e05bbe8d3597468402
TrsNium <umenyan.mitou@gmail.com>
2017-10-13 18:27:37 +0900

    .

--- util.py
+++ util.py
@@ -30,26 +30,40 @@ def read_sentence(read_path):
 
 def convert_sentence2index(sentence, dict_, max_time_step):
     indexs = [dict_.index(word) for word in sentence.split(" ")]
+    num_words = len(indexs)
     while len(indexs) != max_time_step and len(indexs) <= max_time_step:
         indexs.append(len(dict_)) # Add <EOS>
-    return np.array(indexs[:max_time_step])
+    return np.array(indexs[:max_time_step]), num_words
+
+def extract_a_word(list_):
+   pass 
 
 def mk_train_func(batch_size, max_time_step, sentence_read_path, dict_read_path):
     dict_ = read_dict(dict_read_path)
     sentence = np.array(read_sentence(sentence_read_path))
     sentence = sentence[:-(sentence.shape[0]%batch_size)]
 
+    word_num = []
     dump = []
     def func():
         t_len = len(sentence)
         while True:
             if len(dump) != t_len:
-                random_selected_index = random.sample(ange(len(sentence)), batch_size)
-                choiced_s = [convert_sentence2index(s, dict_, max_time_step) for s in sentence[random_selected_index].tolist()]
+                random_selected_index = random.sample(range(len(sentence)), batch_size)
+                converted = [convert_sentence2index(s, dict_, max_time_step) for s in sentence[random_selected_index].tolist()]
+                choiced_s = [tuple_[0] for tuple_ in converted]
                 dump += choiced_s
-                np.delete(converted, random_selected_index)
+                word_num += [tuple_[1] for tuple_ in converted]
+                np.delete(sentence, random_selected_index)
             else:
-                
+                if type(np.array([])) != type(dump):
+                    dump = np.array(dump)
+                    word_num = np.array(word_num)
+
+                choiced_s = dump[random.sample(range(len(dump)), batch_size)].to_lis()
+
+            
+
 
             
             yield 
