4cbba806b6077dcce21475123151c411a4ff560d
TrsNium <umenyan.mitou@gmail.com>
2017-10-16 04:56:02 +0900

    .

--- model.py
+++ model.py
@@ -5,27 +5,28 @@ from util import *
 class model():
     def __init__(self, args):
         self.args = args
-        self.input = tf.placeholder(dtype=tf.int32, shape=(None, args.max_time_step), name="input")
-        self.indices = tf.placeholder(dtype=tf.float32, shape=(args.batch_size), name="indices")
 
-        
         with tf.variable_scope("CBow"):
-            h_in = []
-            self.weight = tf.get_variable("embedding_weight", shape=(args.vocab_size, args.embedding_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-            for t_step in range(args.max_time_step):
-                if t_step != 0:
-                    tf.get_variable_scope().reuse_variables()
+            self.input = tf.placeholder(dtype=tf.int32, shape=(None, args.max_time_step), name="input")
+            self.indices = tf.placeholder(dtype=tf.int32, shape=(args.batch_size), name="indices")
 
-                embedded = tf.nn.embedding_lookup(self.weight, self.input[:, t_step])
-                h_in.append(embedded)
+            with tf.variable_scope("Embedding"):
+                h_in = []
+                self.weight = tf.get_variable("embedding_weight", shape=(args.vocab_size, args.embedding_size), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+                for t_step in range(args.max_time_step):
+                    if t_step != 0:
+                        tf.get_variable_scope().reuse_variables()
+
+                    embedded = tf.nn.embedding_lookup(self.weight, self.input[:, t_step])
+                    h_in.append(embedded)
 
             h_in = tf.reduce_sum(tf.convert_to_tensor(h_in, dtype=tf.float32), axis=0)
             logit = tf.layers.dense(h_in, args.vocab_size)
 
-        target = tf.one_hot(self.indices, args.vocab_size, 1.0, 0.0)
-        print(targe.get_shape().as_list())
+            target = tf.one_hot(self.indices, args.vocab_size, 1.0, 0.0)
+            print(target.get_shape().as_list())
 
-        self.loss = tf.nn.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logit = logit, labels = target))
+            self.loss = tf.nn.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logit = logit, labels = target))
 
     def train(self):
         optimizer = tf.train.GradientDescentOptimizer(self.args.lr).minimize(self.loss)
